e4e691139ce8c7ff247f6780636bb34d4aa197a5
practicedoctor <practicedoctor@users.noreply.github.com>
2014-04-22 21:19:25 -0500

    Create appointments.js

---
+++ js/appointments.js
@@ -0,0 +1,48 @@
+function getAppointments() {
+	var todayDate = new Date();
+	var isoTodayDate = todayDate.toISOString();
+	
+	var otherDate = new Date();
+		$.ajax({
+				type: "GET",
+				url: "http://cse-ilab-08.unl.edu:8282/patient/services/patient_appointment/",
+				dataType: "xml",
+				success: function(xml){
+					$(xml).find('appointment').each(function(){
+						var appointmentDate = $(this).find('date').text();
+						
+						var appDateArray= appointmentDate.split('T');
+						var appFullDate = appDateArray[0];
+						var appFullDateArray = appFullDate.split('-');
+						var year = appFullDateArray[0];
+						var month = appFullDateArray[1];
+						var day = appFullDateArray[2];
+						var time = appDateArray[1];
+												
+						
+						
+						otherDate.setFullYear(year,month-1,day)
+						
+						
+						var task =$(this).find('task');
+						var taskName =$(this).find('taskName').text();
+						//var insertString = "<li>"+ appFullDate + "  <b>Reason</b>  : "+ taskName +"</li>";
+						
+						var insertString = '<tr><td>' + appFullDate+'</td><td>'+ time +'</td><td>' + taskName+'</td><tr>'
+						if (otherDate > todayDate){
+							$('#FutureAppointmentsTable').append(insertString);
+						}
+						else{
+							$('#PreviousAppointmentsTable').append(insertString);
+						}
+						
+						
+																
+						
+						
+					});//end xml.find.each prescriptions
+				}//end success
+			});//end ajax
+		
+     
+     }
